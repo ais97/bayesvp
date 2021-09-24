0f6f8ec74bc754ca01cfe5f6173535c30e7df81d
ais97 <vaiswarya.beec.15@acharya.ac.in>
2021-09-24 10:36:37 +0530

    sample run

---
+++ bayesvp_try.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 15 18:36:44 2021
+
+@author: aiswarya
+"""
+
+#import sys
+#sys.path.append('/home/aiswarya/bayesvp')
+from bayesvp.scripts import bvp_write_config as wc
+from bayesvp.scripts import bvp_process_model as pm
+from bayesvp.scripts import bvpfit as fit
+from bayesvp import config as conf
+from bayesvp import utilities as util 
+from bayesvp import mcmc_setup as setup
+from bayesvp.config import DefineParams as dp
+import matplotlib.pyplot as plt
+
+#import WriteConfig
+
+spec_path = '/home/aiswarya/bayesvp/bayesvp/data/example'
+"""
+config = wc.WriteBayesVPConfig()
+print('line23')
+config.print_to_file(interactive_write=(True))
+print('line25')
+"""
+config_fname = spec_path+'/config_OVI.dat'
+#print('line27')
+
+setup.bvp_mcmc(config_fname)
+#print('line30')
+
+config_fname = spec_path+'/config_OVI2.dat'
+config_params = dp(config_fname)
+#print('line32')
+output = pm.ProcessModel(config_params)
+#print('line34')
+redshift = 0.348
+dv = 300
+
+output.plot_model_comparison(redshift, dv)
+output.corner_plot()
+output.write_model_summary()
+output.write_model_spectrum()
+output.plot_gr_indicator()
+
+
